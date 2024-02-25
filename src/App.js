7f8e3f2af1c92a64b7bf2a1688a6b7fd306c4f87
Szymon Skaskiewicz <szymon.skaskiewicz@gmail.com>
2024-02-25 10:29:04 +0200

    Change: refactored code, added short circuting to utilize useEffect.

--- src/App.js
+++ src/App.js
@@ -1,44 +1,44 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import Stopper from './components/Stopper/Stopper.js';
-
+ 
 function App() {
   const [time, setTime] = useState(0);
-  const [timer, setTimer] = useState(null);
-
-  useEffect(() => {
-    return () => {
-      if (timer) clearInterval(setTimer);
-    };
-  }, [timer])
-
+  const [isRunning, setIsRunning] = useState(false);
  
-
+  useEffect(() => {
+    if (!isRunning) return;
+ 
+    const intervalId = setInterval(() => {
+      setTime(time => time + 10)
+    }, 10)
+ 
+    return () => {
+      clearInterval(intervalId);
+    }
+  }, [isRunning]);
+ 
   const methods = {
     startTimer: () => {
-      if (!timer) {
-        setTimer(
-          setInterval(() => {
-            setTime(time => time + 10)
-          }, 10))
-      }
+      setIsRunning(true);
     },
+ 
     stopTimer: () => {
-      setTimer(null);
-      clearInterval(timer);
+      setIsRunning(false);
     },
+ 
     resetTimer: () => {
+      setIsRunning(false);
       setTime(0)
-      setTimer(null);
-      clearInterval(timer);
     },
   }
-
+ 
   return (
     <div className="App">
       <Stopper {...methods} time={time} />
     </div>
   );
 }
-
+ 
 export default App;
+ 
